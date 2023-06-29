5c1e353ba8ad570547e3ff55c3bac7eacf0d83fb
Elisiane Brand <1134861@atitus.edu.br>
2023-06-28 23:18:13 -0300

    Adicionando menu e ajustes finais

--- space_marker.py
+++ space_marker.py
@@ -1,21 +1,26 @@
 import pygame
 import tkinter as tk
 from tkinter import simpledialog
+import json
 pygame.init()
 
 tamanho = (900, 600)
 preto = (0, 0, 0)
 fundo = pygame.image.load("galaxia.png")
 display = pygame.display.set_mode(tamanho)
+pygame.display.set_caption("SPACE MARKER")
 running = True
-def nomeEstrela(pos):
+def estrelas_marcadas(pos):
     root = tk.Tk()
     root.withdraw()
     nome = simpledialog.askstring("Nome da Estrela", "Digite o nome da estrela:")
     if nome:
         print(f"Nome da estrela: {nome}")
         print(f"Posição do clique: {pos}")
-
+def salvarMarcacoes():
+    with open("marcacoes.json", "w") as arquivo:
+         json.dump(estrelas_marcadas, arquivo)
+         print("As marcações foram salvas com sucesso!")
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,7 +28,11 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Botão esquerdo do mouse
                 mouse_pos = pygame.mouse.get_pos()
-                nomeEstrela(mouse_pos)
+                estrelas_marcadas(mouse_pos)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:  # Tecla ESC para sair
+                salvarMarcacoes()
+                running = False
     display.fill(preto)
     display.blit(fundo, (0, 0))
     pygame.display.update()
